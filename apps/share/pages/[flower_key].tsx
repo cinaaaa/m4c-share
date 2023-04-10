bc0190362ef91db46cfe34593b339de9dfb7a758
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-04-10 16:18:31 +0330

    refactor: commenting

--- apps/share/pages/[flower_key].tsx
+++ apps/share/pages/[flower_key].tsx
@@ -1,54 +1,54 @@
-import Head from 'next/head';
+// Import necessary components and packages
+import Head from "next/head";
 import axios from "axios";
 
+// Define the Page component that receives the image_signed_url prop
 function Page({ image_signed_url }) {
     return (
         <>
+            {/* Set meta tags for the page title, description, and Open Graph properties */}
             <Head>
                 <title>Share Flower</title>
                 <meta name="description" content="Sharing flower" key="desc" />
                 <meta property="og:title" content="Social Title for Cool Page" />
-                <meta
-                    property="og:description"
-                    content="And a social description for our cool page"
-                />
-                <meta
-                    property="og:image"
-                    content={image_signed_url}
-                />
+                <meta property="og:description" content="And a social description for our cool page" />
+                <meta property="og:image" content={image_signed_url} />
             </Head>
+
+            {/* Display the image */}
             <img src={image_signed_url} />
         </>
     );
 }
 
-export async function getServerSideProps(context: { params: { flower_key: any; }; }) {
-    // Fetch image signed url based on key
+// Define the getServerSideProps function, which runs on the server side when the page is requested
+export async function getServerSideProps(context) {
+    // Extract the flower_key from the context"s params
     const { flower_key } = context.params;
     let image_signed_url: string;
-    // send post request to an endpoint with flower_key in the body
-    await axios
-        .post("https://auys8w4hm1.execute-api.eu-central-1.amazonaws.com/Prod/", {
+
+    try {
+        // Send a POST request to the specified API endpoint with the flower_key in the request body
+        const response = await axios.post("https://auys8w4hm1.execute-api.eu-central-1.amazonaws.com/Prod/", {
             path: "/get-signed-url",
             key: flower_key,
-        })
-        .then((response) => {
-            image_signed_url = response.data?.signedUrl;
-        })
-        .catch((error) => {
-            console.log(error);
         });
 
+        // Extract the signed URL from the response
+        image_signed_url = response.data?.signedUrl;
+    } catch (error) {
+        console.log(error);
+    }
 
+    // If the image_signed_url is not available, return a notFound status
     if (!image_signed_url) {
         return {
             notFound: true,
         };
     }
 
+    // Return the image_signed_url as a prop to the Page component
     return { props: { image_signed_url } };
 }
 
 export default Page;
-
-
